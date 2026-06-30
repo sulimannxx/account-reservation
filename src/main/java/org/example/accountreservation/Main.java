930500895981308ba52d4918afe138f3d7b903ea
yurii.sulyma <sulimayura@gmail.com>
2026-06-30 15:06:06 +0400

    UPGYS-4 minor fixes

--- src/main/java/org/example/accountreservation/Main.java
+++ src/main/java/org/example/accountreservation/Main.java
@@ -2,17 +2,9 @@ package org.example.accountreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class Main {
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
